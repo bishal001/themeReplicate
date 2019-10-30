236c3c53859ea40589e23afa76f750be476aa59d
bishal001 <54582823+bishal001@users.noreply.github.com>
2019-10-30 18:15:17 +0545

    Update nav.php

--- nav.php
+++ nav.php
@@ -3,29 +3,36 @@
 
 <body>
   <!-- creating a navigation manu -->
-  <nav class="navbar navbar-expand-lg">
+  <div class="d-none d-lg-block">
+    <nav class="navbar navbar-expand-lg">
+      <a class="navbar-branzd" href="#">Logo</a>
+      <ul class="navbar-nav ml-auto ">
+        <!-- creating nav menu items -->
+        <li class="nav-item">
+          <a href="#" class="nav-link">Home</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link">Our Work</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link">About Us</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link">Services</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link">Our Skills</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link">Contact Us</a>
+        </li>
+      </ul>
+    </nav>
+  </div>  
+
+  <div class="d-lg-none d-block">
+    <nav class="navbar navbar-expand-lg">
     <a class="navbar-branzd" href="#">Logo</a>
-    <ul class="navbar-nav ml-auto">
-      <!-- creating nav menu items -->
-      <li class="nav-item">
-        <a href="#" class="nav-link">Home</a>
-      </li>
-      <li class="nav-item">
-        <a href="#" class="nav-link">Our Work</a>
-      </li>
-      <li class="nav-item">
-        <a href="#" class="nav-link">About Us</a>
-      </li>
-      <li class="nav-item">
-        <a href="#" class="nav-link">Services</a>
-      </li>
-      <li class="nav-item">
-        <a href="#" class="nav-link">Our Skills</a>
-      </li>
-      <li class="nav-item">
-        <a href="#" class="nav-link">Contact Us</a>
-      </li>
-    </ul>
-  </nav>
-  <br>
+    </nav>
+  </div>
   
