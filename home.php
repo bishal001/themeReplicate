3ceb613456302df54198bd7c3840f79f1d016e06
bishal001 <54582823+bishal001@users.noreply.github.com>
2019-10-30 16:56:16 +0545

    added blinking text in the hero section

--- home.php
+++ home.php
@@ -7,11 +7,18 @@
     <div class="col-xl-12 col-lg-12 col-md-12 font-weight-bold hero_text text-center">turtle</div>
     <!-- div to display the partical effect -->
     <div id="particles-js" class="mx-auto"></div>
+
+    <!-- blinking text effect section -->
+    <div class="flex">
+      <p id="word" class="font-weight-bold"></p><p class="blink">|</p>
+    </div>
   </div>
 </div>
 <!-- hero section ended -->
 
+
+<script src="javascript/custom.js"></script>
 <!-- calling js files for the particles -->
 <script src="javascript/particles.js"></script>
-    <script src="javascript/app.js"></script>
+<script src="javascript/app.js"></script>
 </body>
