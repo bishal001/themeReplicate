e1536c779cd1d6a07ee9cf447b87a3dfc5cc5437
bishal001 <54582823+bishal001@users.noreply.github.com>
2019-11-04 13:14:16 +0545

    added the side navigation menu

--- nav.php
+++ nav.php
@@ -1,42 +1,180 @@
-<!-- creating a navigation manu -->
-<p style="color: #eaeaea;font-size: 40px;font-weight: bold;" class="text-center logo"><img src="images/logo.png" class="img-fluid" alt="">Turtle</p>
-<nav class="navbar navbar-expand-lg" id="mySidenav">
-
-  <a href="javascript:void(0)" class="closebtn d-block d-lg-none" onclick="closeNav()">&times;</a>
-  <a class="navbar-brand" href="#"><img src="images/logo.png" class="img-fluid" alt="">Turtle</a>
-  <ul class="navbar-nav ml-auto ">
-    <!-- creating nav menu items -->
-    <li class="nav-item">
-      <a href="#" class="nav-link underline">Home</a>
-    </li>
-    <li class="nav-item">
-      <a href="#" class="nav-link underline">Our Work</a>
-    </li>
-    <li class="nav-item">
-      <a href="#" class="nav-link underline">About Us</a>
-    </li>
-    <li class="nav-item">
-      <a href="#" class="nav-link underline">Services</a>
-    </li>
-    <li class="nav-item">
-      <a href="#" class="nav-link underline">Our Skills</a>
-    </li>
-    <li class="nav-item">
-      <a href="#" class="nav-link underline">Contact Us</a>
-    </li>
-  </ul>
-  <span style="font-size:30px;cursor:pointer;" class="ml-auto nav_open" onclick="openNav()">&#9776;
-  </span>
-</nav>
-
-
-<!-- js code for the side menu toogle -->
-<script>
-function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
+<style>
+.animated-icon3 {
+  width: 30px;
+  height: 20px;
+  position: relative;
+  margin: 0px;
 }
 
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
+.animated-icon3 span {
+  display: block;
+  position: absolute;
+  height: 3px;
+  width: 100%;
+  border-radius: 9px;
+  opacity: 1;
+  left: 0;
+  -webkit-transform: rotate(0deg);
+  -moz-transform: rotate(0deg);
+  -o-transform: rotate(0deg);
+  transform: rotate(0deg);
+  -webkit-transition: .25s ease-in-out;
+  -moz-transition: .25s ease-in-out;
+  -o-transition: .25s ease-in-out;
+  transition: .25s ease-in-out;
 }
-</script>
+
+
+.animated-icon3 span {
+  background: #f3e5f5;
+}
+
+
+/* Icon 4 */
+
+.animated-icon3 span:nth-child(1) {
+  top: 0px;
+  -webkit-transform-origin: left center;
+  -moz-transform-origin: left center;
+  -o-transform-origin: left center;
+  transform-origin: left center;
+}
+
+.animated-icon3 span:nth-child(2) {
+  top: 10px;
+  -webkit-transform-origin: left center;
+  -moz-transform-origin: left center;
+  -o-transform-origin: left center;
+  transform-origin: left center;
+}
+
+.animated-icon3 span:nth-child(3) {
+  top: 20px;
+  -webkit-transform-origin: left center;
+  -moz-transform-origin: left center;
+  -o-transform-origin: left center;
+  transform-origin: left center;
+}
+
+.animated-icon3.open span:nth-child(1) {
+  -webkit-transform: rotate(45deg);
+  -moz-transform: rotate(45deg);
+  -o-transform: rotate(45deg);
+  transform: rotate(45deg);
+  top: 0px;
+  left: 8px;
+}
+
+.animated-icon3.open span:nth-child(2) {
+  width: 0%;
+  opacity: 0;
+}
+
+.animated-icon3.open span:nth-child(3) {
+  -webkit-transform: rotate(-45deg);
+  -moz-transform: rotate(-45deg);
+  -o-transform: rotate(-45deg);
+  transform: rotate(-45deg);
+  top: 21px;
+  left: 8px;
+}
+
+.side_menu {
+  position: fixed;
+  bottom: 0;
+  right: 0;
+  margin-bottom: 30;
+  margin-right: 30;
+}
+
+.menu_items{
+  width: 25%;
+  position: fixed;
+  bottom: 0;
+  right: 0;
+  transform: translate(0%,-20%);
+  margin-right: 90;
+  background-color:black;
+  padding-left: 10;
+}
+
+.side-nav{
+  position: absolute;
+}
+
+</style>
+<!-- creating a navigation manu for the large screen -->
+<div class="d-lg-block d-none">
+  <nav class="navbar navbar-expand-lg">
+    <a class="navbar-brand" href="#"><img src="images/logo.png" class="img-fluid" alt="">Turtle</a>
+    <ul class="navbar-nav ml-auto">
+      <!-- creating nav menu items -->
+      <li class="nav-item">
+        <a href="#" class="nav-link underline ml-3">Home</a>
+      </li>
+      <li class="nav-item">
+        <a href="#" class="nav-link underline">Our Work</a>
+      </li>
+      <li class="nav-item">
+        <a href="#" class="nav-link underline">About Us</a>
+      </li>
+      <li class="nav-item">
+        <a href="#" class="nav-link underline">Services</a>
+      </li>
+      <li class="nav-item">
+        <a href="#" class="nav-link underline">Our Skills</a>
+      </li>
+      <li class="nav-item">
+        <a href="#" class="nav-link underline">Contact Us</a>
+      </li>
+    </ul>
+    </span>
+  </nav>
+</div>
+
+<!-- creating a navigation manu for the small screen -->
+<div class="d-lg-none d-block">
+
+  <div class="sticky-top side-nav justify-content-center">
+    <!-- Collapse button -->
+    <button class="navbar-toggler ml-auto side_menu button" type="button" data-toggle="collapse"
+      data-target="#navbarSupportedContent22" aria-controls="navbarSupportedContent22" aria-expanded="false"
+      aria-label="Toggle navigation">
+      <div class="animated-icon3 "><span></span><span></span><span></span></div>
+    </button>
+    <!-- contains the body section of the nav bar for the small device -->
+    <div class="collapse navbar-collapse" id="navbarSupportedContent22">
+      <ul class="navbar-nav menu_items ml-4">
+        <li class="nav-item">
+          <a href="#" class="nav-link underline">Home</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link underline">Our Work</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link underline">About Us</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link underline">Services</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link underline">Our Skills</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link underline">Contact Us</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+   <!-- js code for the side menu toogle -->
+  <script>
+  $(document).ready(function() {
+
+$('.button').on('click', function() {
+
+  $('.animated-icon3').toggleClass('open');
+});
+});
+  </script>
+</div>
+</div>
--- side_nav.php
+++ side_nav.php
@@ -1,42 +1,159 @@
-<!-- css code for the side nav bar -->
 <style>
-/* body part of the side nav bar */
+/* Icon 1 */
 
+.animated-icon3 {
+  width: 30px;
+  height: 20px;
+  position: relative;
+  margin: 0px;
+}
+
+.animated-icon3 span {
+  display: block;
+  position: absolute;
+  height: 3px;
+  width: 100%;
+  border-radius: 9px;
+  opacity: 1;
+  left: 0;
+  -webkit-transform: rotate(0deg);
+  -moz-transform: rotate(0deg);
+  -o-transform: rotate(0deg);
+  transform: rotate(0deg);
+  -webkit-transition: .25s ease-in-out;
+  -moz-transition: .25s ease-in-out;
+  -o-transition: .25s ease-in-out;
+  transition: .25s ease-in-out;
+}
+
+
+.animated-icon3 span {
+  background: #f3e5f5;
+}
+
+
+/* Icon 4 */
+
+.animated-icon3 span:nth-child(1) {
+  top: 0px;
+  -webkit-transform-origin: left center;
+  -moz-transform-origin: left center;
+  -o-transform-origin: left center;
+  transform-origin: left center;
+}
+
+.animated-icon3 span:nth-child(2) {
+  top: 10px;
+  -webkit-transform-origin: left center;
+  -moz-transform-origin: left center;
+  -o-transform-origin: left center;
+  transform-origin: left center;
+}
+
+.animated-icon3 span:nth-child(3) {
+  top: 20px;
+  -webkit-transform-origin: left center;
+  -moz-transform-origin: left center;
+  -o-transform-origin: left center;
+  transform-origin: left center;
+}
+
+.animated-icon3.open span:nth-child(1) {
+  -webkit-transform: rotate(45deg);
+  -moz-transform: rotate(45deg);
+  -o-transform: rotate(45deg);
+  transform: rotate(45deg);
+  top: 0px;
+  left: 8px;
+}
+
+.animated-icon3.open span:nth-child(2) {
+  width: 0%;
+  opacity: 0;
+}
+
+.animated-icon3.open span:nth-child(3) {
+  -webkit-transform: rotate(-45deg);
+  -moz-transform: rotate(-45deg);
+  -o-transform: rotate(-45deg);
+  transform: rotate(-45deg);
+  top: 21px;
+  left: 8px;
+}
+
+.side_menu {
+  position: fixed;
+  bottom: 0;
+  right: 0;
+  margin-bottom: 30;
+  margin-right: 30;
+}
+
+.menu_items{
+  width: 25%;
+  position: fixed;
+  bottom: 0;
+  right: 0;
+  transform: translate(0%,-20%);
+  margin-right: 90;
+}
 </style>
-<!-- creating the side nav menu -->
-<div class="row d-block d-lg-none">
-  <div class="col-12 text-center">
-    sds
-  </div>
+
+<?php include'header.php'?>
+<!--Navbar-->
+<div sticky-top side-nav justify-content-center>
+  <!--Navbar-->
+  <nav class="navbar navbar-dark  indigo darken-2">
+
+    <!-- Navbar brand -->
+
+
+    <!-- Collapse button -->
+    <button class="navbar-toggler ml-auto side_menu button" type="button" data-toggle="collapse"
+      data-target="#navbarSupportedContent22" aria-controls="navbarSupportedContent22" aria-expanded="false"
+      aria-label="Toggle navigation">
+      <div class="animated-icon3 "><span></span><span></span><span></span></div>
+    </button>
+
+    <!-- Collapsible content -->
+    <div class="collapse navbar-collapse" id="navbarSupportedContent22">
+
+      <!-- Links -->
+      <ul class="navbar-nav menu_items ml-4" style="background-color:black">
+        <li class="nav-item">
+          <a href="#" class="nav-link underline">Home</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link underline">Our Work</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link underline">About Us</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link underline">Services</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link underline">Our Skills</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link underline">Contact Us</a>
+        </li>
+      </ul>
+      <!-- Links -->
+
+    </div>
+    <!-- Collapsible content -->
+
+  </nav>
+  <!--/.Navbar-->
 </div>
-<div class="d-block d-lg-none sticky-top side-nav justify-content-center">
-  <div id="mySidenav" class="sidenav">
-    <!-- contains the cart, login and register section -->
-    
-    <!-- contains the body section of the nav bar for the small device -->
-    <ul class="navbar-nav ml-4">
-      <li class="nav-item">
-        <a href="#" class="nav-link underline">Home</a>
-      </li>
-      <li class="nav-item">
-        <a href="#" class="nav-link underline">Our Work</a>
-      </li>
-      <li class="nav-item">
-        <a href="#" class="nav-link underline">About Us</a>
-      </li>
-      <li class="nav-item">
-        <a href="#" class="nav-link underline">Services</a>
-      </li>
-      <li class="nav-item">
-        <a href="#" class="nav-link underline">Our Skills</a>
-      </li>
-      <li class="nav-item">
-        <a href="#" class="nav-link underline">Contact Us</a>
-      </li>
-    </ul>
 
+<script>
+$(document).ready(function() {
 
-    
-  </div>
+  $('.button').on('click', function() {
 
-</div>
+    $('.animated-icon3').toggleClass('open');
+  });
+});
+</script>
