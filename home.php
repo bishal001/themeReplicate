272265e0bde7efa7b73917fc990a150b7f0aec69
bishal001 <54582823+bishal001@users.noreply.github.com>
2019-11-05 16:38:26 +0545

    added hero section

--- home.php
+++ home.php
@@ -1,27 +1,30 @@
 <!-- calling the header section -->
 <?php include'header.php'?>
 
+<!-- Section 1 started -->
+<section>
+  <!-- hero section started -->
+  <div class="hero">
+    <div class="hero_layer">
+      <!-- calling nav menu -->
+      <?php include'nav.php'?>
 
-<!-- hero section started -->
-<div class="hero">
-  <div class="hero_layer">
-    <!-- calling nav menu -->
-    <?php include'nav.php'?>
+      <!-- hero section text -->
+      <div class="col-xl-12 col-lg-12 col-md-12 font-weight-bold hero_text text-center">turtle</div>
+      <!-- div to display the partical effect -->
+      <div id="particles-js" class="mx-auto"></div>
 
-    <!-- hero section text -->
-    <div class="col-xl-12 col-lg-12 col-md-12 font-weight-bold hero_text text-center">turtle</div>
-    <!-- div to display the partical effect -->
-    <div id="particles-js" class="mx-auto"></div>
-
-    <!-- blinking text effect section -->
-    <div class="flex" style="width:100%">
-      <p id="word" class="font-weight-bold typed_text"></p>
-      <p class="blink typed_text">|</p>
+      <!-- blinking text effect section -->
+      <div class="flex" style="width:100%">
+        <p id="word" class="font-weight-bold typed_text"></p>
+        <p class="blink typed_text">|</p>
+      </div>
     </div>
   </div>
-</div>
+  <!-- hero section ended -->
+</section>
+<!-- Section 1 en -->
 
-<!-- hero section ended -->
 
 <!-- importing custom js file -->
 <script src="javascript/custom.js"></script>
