b23c1f4d9bf8ab6356a7c08947fe938ca7bbb073
bishal001 <54582823+bishal001@users.noreply.github.com>
2019-11-05 17:00:37 +0545

    started section 2

--- home.php
+++ home.php
@@ -2,29 +2,31 @@
 <?php include'header.php'?>
 
 <!-- Section 1 started -->
-<section>
-  <!-- hero section started -->
-  <div class="hero">
-    <div class="hero_layer">
-      <!-- calling nav menu -->
-      <?php include'nav.php'?>
+<section class="row hero no-gutters">
+  <div class="hero_layer">
+    <!-- calling nav menu -->
+    <?php include'nav.php'?>
 
-      <!-- hero section text -->
-      <div class="col-xl-12 col-lg-12 col-md-12 font-weight-bold hero_text text-center">turtle</div>
-      <!-- div to display the partical effect -->
-      <div id="particles-js" class="mx-auto"></div>
+    <!-- hero section text -->
+    <div class="col-xl-12 col-lg-12 col-md-12 font-weight-bold hero_text text-center">turtle</div>
+    <!-- div to display the partical effect -->
+    <div id="particles-js" class="mx-auto"></div>
 
-      <!-- blinking text effect section -->
-      <div class="flex" style="width:100%">
-        <p id="word" class="font-weight-bold typed_text"></p>
-        <p class="blink typed_text">|</p>
-      </div>
+    <!-- blinking text effect section -->
+    <div class="flex" style="width:100%">
+      <p id="word" class="font-weight-bold typed_text"></p>
+      <p class="blink typed_text">|</p>
     </div>
   </div>
   <!-- hero section ended -->
 </section>
 <!-- Section 1 en -->
 
+<!-- started section 2 -->
+<section class="row section-2 no-gutters">
+  
+</section>
+<!-- ended section 2 -->
 
 <!-- importing custom js file -->
 <script src="javascript/custom.js"></script>
