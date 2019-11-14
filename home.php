8b424a22e8d8885142402b10fb0d39f632fa9e86
bishal001 <54582823+bishal001@users.noreply.github.com>
2019-11-14 22:20:46 +0545

    a

--- home.php
+++ home.php
@@ -191,6 +191,7 @@
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio.
       Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet.
       Duis sagittis ipsum. Praesent mauris.</p>
+    
   </div>
 </section>
 <!-- ended section 6 -->
